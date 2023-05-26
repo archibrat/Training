0e98829dfafeda74d605b068638d4c6bf1d90c10
archibrat <30520653+archibrat@users.noreply.github.com>
2023-05-26 20:09:49 +0300

    Update Challenge. Transpose two-dimensional arrays.sql

--- Codewars/SQL/Challenge. Transpose two-dimensional arrays.sql
+++ Codewars/SQL/Challenge. Transpose two-dimensional arrays.sql
@@ -1,43 +1,39 @@
-/*SELECT
-  ARRAY(
-    SELECT (
-      ARRAY (
-        SELECT matrix[j][s]
-        FROM ( SELECT generate_subscripts(matrix, 1) AS j ) foo
-      )
-    ) 
-    FROM ( SELECT generate_subscripts(matrix, 2) AS s ) bar
-  ) as matrix
-FROM public.matrices m
-*/
-
-SELECT  array_agg(v ORDER BY j) matrix
-FROM 
+/*SELECT ARRAY(
+SELECT  ( ARRAY (
+SELECT  matrix[j][s]
+FROM
 (
-	SELECT  rn
-	       ,j
+	SELECT  generate_subscripts(matrix,1) AS j
+) foo ) )
+FROM
+(
+	SELECT  generate_subscripts(matrix,2) AS s
+) bar ) AS matrix
+FROM public.matrices m */
+SELECT  array_agg(v ORDER BY j) matrix
+FROM
+(
+	SELECT  rn, j
 	       ,array_agg(v ORDER BY i) AS v
-	FROM 
+	FROM
 	(
-		SELECT  rn
-		       ,i
-		       ,j
-		       ,matrix[i][j] AS v
-		FROM 
+		SELECT  rn,i,j
+		       ,matrix [i] [j] AS v
+		FROM
 		(
 			SELECT  generate_subscripts(matrix,2) j
 			       ,q.*
-			FROM 
+			FROM
 			(
-				SELECT  row_number() over()           AS rn
+				SELECT  row_number() OVER ()          AS rn
 				       ,generate_subscripts(matrix,1) AS i
 				       ,matrix
 				FROM matrices
-			) q 
-		) r 
-	)s
+			) q
+		) r
+	) s
 	GROUP BY  rn
-	         ,j 
+	         ,j
 ) t
 GROUP BY  rn
 ORDER BY rn;
